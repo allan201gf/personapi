983860cb58672cb4c8b0ad76d1ce42fa32bbbd49
allan201gf <allan201gf@gmail.com>
2021-08-12 09:53:57 -0300

    api test

---
+++ src/main/java/com/allan201gf/personapi/PersonapiApplication.java
@@ -0,0 +1,13 @@
+package com.allan201gf.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
